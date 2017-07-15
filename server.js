1ecd83e2004170dc1fcd4b4ba3dff3d49287e4f6
Wai-Yin Kwan <wykhuh@icloud.com>
2017-07-15 07:31:34 -0700

    require authentication for api

--- server.js
+++ server.js
@@ -3,10 +3,32 @@ var server = jsonServer.create();
 var router = jsonServer.router('db.json');
 var middlewares = jsonServer.defaults();
 
+var env = process.env.NODE_ENV || 'development'
+// use dotenv during development
+if(env === 'development') {
+  require('dotenv').config();
+}
+
 server.use(middlewares);
 
+// check if the url contains the secret as a query string
+// ex: /users?secret=xxxx
+function isAuthorized(req) {
+  return req.query.secret === process.env.API_SECRET
+}
+
+server.use((req, res, next) => {
+  // if request is authorized, go to next step
+  if (isAuthorized(req)) {
+    next()
+  // if it is not authorized, show error
+  } else {
+    res.sendStatus(401)
+  }
+})
+
 // prepend all routes with '/api'
-// for example '/api/users'
+// ex: '/api/users'
 server.use('/api', router);
 
 // start server
